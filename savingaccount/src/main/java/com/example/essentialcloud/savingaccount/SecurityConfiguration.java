0459bf83a8ff73b8ccda5b0444da8664022bbae5
Karl Nicholas <karl.nicholas@outlook.com>
2023-12-28 12:17:52 -0500

    General cleanup

--- savingaccount/src/main/java/com/example/essentialcloud/savingaccount/SecurityConfiguration.java
+++ savingaccount/src/main/java/com/example/essentialcloud/savingaccount/SecurityConfiguration.java
@@ -4,10 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SavingAccountConfiguration {
+@EnableWebSecurity
+public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -15,6 +18,8 @@ public class SavingAccountConfiguration {
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-        return http.build();
+        return http.sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
     }
 }
--- userinfo/src/main/java/com/example/essentialcloud/userinfo/UserinfoApplication.java
+++ userinfo/src/main/java/com/example/essentialcloud/userinfo/UserinfoApplication.java
@@ -4,13 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,11 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @RequestMapping("/api/v1/")
-@EnableCaching
-@Configuration
-@EnableWebSecurity
 public class UserinfoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(UserinfoApplication.class, args);
 	}
@@ -35,12 +24,4 @@ public class UserinfoApplication {
 		return userInfoService.retrieveUserInfoByAuthenticationId(authenticationId);
 	}
 
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests(authorize -> authorize
-				.anyRequest().authenticated()
-			)
-			.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
-		return http.build();
-	}
 }
